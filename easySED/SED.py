9a02585e285493b1f97677735853b55f392f0d39
harsh gupta <harshnative@gmail.com>
2020-10-07 08:48:54 +0530

    final encryption system tested and updated

--- easySED/SED.py
+++ easySED/SED.py
@@ -3,68 +3,31 @@ import onetimepad
 import copy
 import hashlib
 
-
-def isSubString(string, subString):
-    lengthOfSubString = len(subString)
-    try:
-        for i, j in enumerate(string):
-            if(j == subString[0]):
-                if(subString == string[i:i+lengthOfSubString]):
-                    return True
-                else:
-                    pass
-        return False
-    except Exception as e:
-        return False
-
-
-
 class ED:
 
     def __init__(self):
 
         # pin for salting password key
         self.__pin = None
-
         self.__password = None
+        self.__convPass = None
 
-        # salting list
-        self.saltList = ["This" , "is" , "an" , "industry" , "level" , "encryption"]
-
-        # for salting fernet key
-        self.keySalt1 = None
-        self.keySalt2 = None
-
-        self.securityLevelHigh = True
-        self.outpass = False
-
-    
-    # function to set your own list
-    def setOwnSaltList(self , saltList):
-        if(len(saltList) != 6):
-            raise Exception("please pass 6 strings in list")
-
-        else:
-            self.saltList = saltList
-
-
-
-    # function for checking if a password contain all lower , upper , nums and special case characters and also if is of 12 digit or not 
-    def checkPass(self , string):
+    # function for checking if a password contain all lower , upper , nums and special case characters and also if is of required length or not 
+    def checkPass(self , string , minLength = 8 , lowerCase = True , upperCase = True , nums = True , specialChar = True):
 
         # checking for 12 digit length 
-        if(len(string) != 12):
+        if(len(string) < minLength):
             return False
 
         # lower case list
-        lowerCase = ['a','s','d','f','g','h','j','k','l','z','x',
+        lowerCaseList = ['a','s','d','f','g','h','j','k','l','z','x',
                     'c','v','b','n','m','q','w','e','r','t','y','u'
                     ,'i','o','p']
 
         # upper case list 
-        upperCase = []
-        for i in lowerCase:
-            upperCase.append(i.upper())
+        upperCaseList = []
+        for i in lowerCaseList:
+            upperCaseList.append(i.upper())
 
 
         # nums and special characters
@@ -72,7 +35,7 @@ class ED:
                 '`','/','+','/','<','>','[',']','{','}','.',':',';',
                 '|','#']
 
-        nums = ['1','2','3','4','5','6','7','8','9','0']
+        numsList = ['1','2','3','4','5','6','7','8','9','0']
 
         # tempList for checking if contains lowers uppers etc
         tempList = []
@@ -91,88 +54,73 @@ class ED:
                 tempList.append("s")
 
             # if special char is present
-            elif(s in nums):
+            elif(s in numsList):
                 tempList.append("n")
 
-        count = 0
+
         if("l" in tempList):
-            count = count + 1
-        
+            if(not(lowerCase)):
+                return False
+
         if("u" in tempList):
-            count = count + 1
+            if(not(upperCase)):
+                return False
 
         if("s" in tempList):
-            count = count + 1
+            if(not(specialChar)):
+                return False
 
         if("n" in  tempList):
-            count = count + 1 
-
-        if(count >=  4):
-            return True
-        else:
-            return False
-
-
-    
-    # function for checking if the pin is of 6 digit or not 
-    def checkPin(self , pin):
-
-        if(len(pin) != 6):
-            return False
+            if(not(nums)):
+                return False
 
         return True
 
     
-    def setSecurityLevel_toLow(self):
-        self.securityLevelHigh = False
         
 
-    # function for setting the password , pin , keysalt
-    # password must contain all lower , upper , nums , sp chars
-    # pin must be of 6 digit
-    def setPassword_Pin_keySalt(self , password , pin = 123456 , keySalt = "harshnative"):
+    # function for setting the password , pin
+    def setPassword_Pin(self , password , pin = 123456):
 
-        password = str(password)
-        pin = str(pin)
-        keySalt = str(keySalt)
-        
-        if(self.securityLevelHigh):
-            if(self.checkPass(password)):
-                self.__password = str(copy.copy(password))
-            else:
-                raise Exception("please set a 12 digit pass containing at least lower , upper , nums , special character \nOr you can set the security level to low")
-        
-        else:
-            self.__password = str(copy.copy(password))
-        
-        if(self.checkPin(pin)):
-            self.__pin = str(copy.copy(pin))
-        else:
-            raise Exception("please set a 6 digit pin \nOr you can set the security level to low")
+        if(len(str(pin)) > 6):
+            raise Exception("pin cannot be more than 6 digit long")
 
-
-        
-        lenKeySalt = round(len(keySalt) / 2 )
-
-        self.keySalt1 = keySalt[:lenKeySalt]
-        self.keySalt2 = keySalt[lenKeySalt:]
-
+        self.__pin = copy.copy(int(pin))
+        self.__password = str(copy.copy(self.getEncryptedPassword(str(password))))
+    
+        self.__convPass = self.convPassword()
 
     # function to add salt to the password
     def convPassword(self):
-        if(self.__password == None):
-            raise Exception("please set the password")
-
-        count = 0
         
-        newPass = self.__password
+        count = 1  
+        myList = []
+        tempString = ""
 
-        for i in self.__pin:
-            digit = int(i)
-            newPass = newPass[:digit] + self.saltList[count] + newPass[digit:]
+        for i in self.__password:
+            tempString = tempString + i
+
+            if(count == 4):
+                count = 1
+                myList.append(tempString)
+                tempString = ""
+                continue
+            
             count += 1
 
-        return newPass
+        convPass = ""
+
+        for i in str(self.__pin):
+            convPass = convPass + myList.pop(int(i))
+
+        for i in myList:
+            convPass = convPass + i
+
+        return convPass
+
+        
+            
+
 
 
     # function to check if everything is set up
@@ -183,8 +131,8 @@ class ED:
         if(self.__pin == None):
             raise Exception("please set pin")
 
-        if((self.keySalt1 == None) and (self.keySalt2 == None)):
-            raise Exception("please set key salt")
+        if(self.__convPass == None):
+            raise Exception("please set password and pin")
     
 
     
@@ -195,8 +143,6 @@ class ED:
             
         stringToReturn = ""
 
-        convPass = self.convPassword()
-
         # key
         key = Fernet.generate_key()
         
@@ -204,8 +150,7 @@ class ED:
         newKey = key.decode("utf-8")
 
         # encryting the key using the conv pass and keysalts
-        keyToAdd = onetimepad.encrypt(newKey , self.keySalt1 + convPass + self.keySalt2)
-
+        keyToAdd = onetimepad.encrypt(newKey , self.__convPass)
 
         # conv string to bytes
         stringToPass = bytes(stringToEncrypt , "utf-8")
@@ -225,10 +170,8 @@ class ED:
 
         self.checkIfPossible()
 
-        convPass = self.convPassword()
-
         # getting the key
-        newKey = onetimepad.decrypt(stringToDecrypt[:88] , self.keySalt1 + convPass + self.keySalt2)
+        newKey = onetimepad.decrypt(stringToDecrypt[:88] , self.__convPass)
 
         # conv strings to bytes
         key = bytes(newKey , "utf-8")
@@ -240,35 +183,72 @@ class ED:
 
 
     
-    def returnEncryptedPassword(self , password):
+    def getEncryptedPassword(self , password):
         sha_signature = hashlib.sha256(password.encode()).hexdigest()
-        return sha_signature.decode("utf-8")
+        return sha_signature
 
+    
+    def returnPassForStoring(self):
+        return self.__convPass
 
-    def authenticatePassword(self, passwordInput , hashedPassword):
-        sha_signature = hashlib.sha256(passwordInput.encode()).hexdigest()
+    def authenticatePassword(self, storedPass , passwordInput , pinInput = 123456):
+        
+        tempPass = self.__password
+        tempPin = self.__pin
+        tempConvPass = self.__convPass
 
-        if(sha_signature == hashedPassword):
+        self.setPassword_Pin(passwordInput , pinInput)
+
+        if(str(self.__convPass) == str(storedPass)):
+            self.__password = tempPass
+            self.__pin = tempPin
+            self.__convPass = tempConvPass
+
+            del tempPass
+            del tempPin
+            del tempConvPass
+
             return True
+            
         else:
+            self.__password = tempPass
+            self.__pin = tempPin
+            self.__convPass = tempConvPass
+
+            del tempPass
+            del tempPin
+            del tempConvPass
+
             return False
+
 
 
 
 if __name__ == "__main__":
     e = ED()
 
-    saltList = ["This" , "is" , "an" , "industry" , "level" , "encryption"]
+    e.setPassword_Pin("#123hello" , "236598")
 
-    e.setSecurityLevel_toLow()
-    e.setPassword_Pin_keySalt("#123" , "236598" , "letscodeofficial.com")
-    e.setOwnSaltList(saltList)
-
-    encoded  = e.encrypter("hello world")
-    print(encoded)
+    encoded  = e.encrypter("hello world , my name is harsh native and I love programming")
+    print("encypted string = " , encoded)
     decoded = e.decrypter(encoded)
-    print(decoded)
+    print("\ndecrypted string = " , decoded)
 
+    # password you can store for further authentication 
+    objStore = e.returnPassForStoring()
+
+    print("\npassword you can store for further authentication = " , objStore)
+
+    if(e.authenticatePassword(objStore , "#123hello" , "236598")):
+        print("\nyou are rigth user")
+    
+    if(e.authenticatePassword(objStore , "#123world" , "236598")):
+        print("\nshould not be here")
+    else:
+        print("\nwrong user")
+
+
+   
     
 
         
--- easySED/test.py
+++ easySED/test.py
@@ -1,6 +1,7 @@
 from cryptography.fernet import Fernet
 import onetimepad
 import copy
+import hashlib
 
 
 def isSubString(string, subString):
@@ -24,49 +25,25 @@ class ED:
 
         # pin for salting password key
         self.__pin = None
-
         self.__password = None
+        self.__convPass = None
 
-        # salting list
-        self.saltList = ["This" , "is" , "an" , "industry" , "level" , "encryption"]
-
-        # for salting fernet key
-        self.keySalt1 = None
-        self.keySalt2 = None
-
-        self.securityLevelHigh = True
-        self.outpass = False
-
-    
-    # function to output the encrypted password as well in the outputted string
-    def setOutPutPass(self):
-        self.outpass = True
-
-    def setOwnSaltList(self , saltList):
-        if(len(saltList) != 6):
-            raise Exception("please pass 6 strings in list")
-
-        else:
-            self.saltList = saltList
-
-
-
-    # function for checking if a password contain all lower , upper , nums and special case characters and also if is of 12 digit or not 
-    def checkPass(self , string):
+    # function for checking if a password contain all lower , upper , nums and special case characters and also if is of required length or not 
+    def checkPass(self , string , minLength = 8 , lowerCase = True , upperCase = True , nums = True , specialChar = True):
 
         # checking for 12 digit length 
-        if(len(string) != 12):
+        if(len(string) < minLength):
             return False
 
         # lower case list
-        lowerCase = ['a','s','d','f','g','h','j','k','l','z','x',
+        lowerCaseList = ['a','s','d','f','g','h','j','k','l','z','x',
                     'c','v','b','n','m','q','w','e','r','t','y','u'
                     ,'i','o','p']
 
         # upper case list 
-        upperCase = []
-        for i in lowerCase:
-            upperCase.append(i.upper())
+        upperCaseList = []
+        for i in lowerCaseList:
+            upperCaseList.append(i.upper())
 
 
         # nums and special characters
@@ -74,7 +51,7 @@ class ED:
                 '`','/','+','/','<','>','[',']','{','}','.',':',';',
                 '|','#']
 
-        nums = ['1','2','3','4','5','6','7','8','9','0']
+        numsList = ['1','2','3','4','5','6','7','8','9','0']
 
         # tempList for checking if contains lowers uppers etc
         tempList = []
@@ -93,88 +70,73 @@ class ED:
                 tempList.append("s")
 
             # if special char is present
-            elif(s in nums):
+            elif(s in numsList):
                 tempList.append("n")
 
-        count = 0
+
         if("l" in tempList):
-            count = count + 1
-        
+            if(not(lowerCase)):
+                return False
+
         if("u" in tempList):
-            count = count + 1
+            if(not(upperCase)):
+                return False
 
         if("s" in tempList):
-            count = count + 1
+            if(not(specialChar)):
+                return False
 
         if("n" in  tempList):
-            count = count + 1 
-
-        if(count >=  4):
-            return True
-        else:
-            return False
-
-
-    
-    # function for checking if the pin is of 6 digit or not 
-    def checkPin(self , pin):
-
-        if(len(pin) != 6):
-            return False
+            if(not(nums)):
+                return False
 
         return True
 
     
-    def setSecurityLevel_toLow(self):
-        self.securityLevelHigh = False
         
 
-    # fucntion for setting the password , pin , keysalt
-    # password must contain all lower , upper , nums , sp chars
-    # pin must be of 6 digit
-    def setPassword_Pin_keySalt(self , password , pin = 123456 , keySalt = "harshnative"):
+    # function for setting the password , pin
+    def setPassword_Pin(self , password , pin = 123456):
 
-        password = str(password)
-        pin = str(pin)
-        keySalt = str(keySalt)
-        
-        if(self.securityLevelHigh):
-            if(self.checkPass(password)):
-                self.__password = str(copy.copy(password))
-            else:
-                raise Exception("please set a 12 digit pass containing at least lower , upper , nums , special character \nOr you can set the security level to low")
-        
-        else:
-            self.__password = str(copy.copy(password))
-        
-        if(self.checkPin(pin)):
-            self.__pin = str(copy.copy(pin))
-        else:
-            raise Exception("please set a 6 digit pin \nOr you can set the security level to low")
+        if(len(str(pin)) > 6):
+            raise Exception("pin cannot be more than 6 digit long")
 
-
-        
-        lenKeySalt = len(keySalt)
-        self.keySalt1 = keySalt[:lenKeySalt]
-        self.keySalt2 = keySalt[lenKeySalt:]
-
-
+        self.__pin = copy.copy(int(pin))
+        self.__password = str(copy.copy(self.getEncryptedPassword(str(password))))
+    
+        self.__convPass = self.convPassword()
 
     # function to add salt to the password
     def convPassword(self):
-        if(self.__password == None):
-            raise Exception("please set the password")
-
-        count = 0
         
-        newPass = self.__password
+        count = 1  
+        myList = []
+        tempString = ""
 
-        for i in self.__pin:
-            digit = int(i)
-            newPass = newPass[:digit] + self.saltList[count] + newPass[digit:]
+        for i in self.__password:
+            tempString = tempString + i
+
+            if(count == 4):
+                count = 1
+                myList.append(tempString)
+                tempString = ""
+                continue
+            
             count += 1
 
-        return newPass
+        convPass = ""
+
+        for i in str(self.__pin):
+            convPass = convPass + myList.pop(int(i))
+
+        for i in myList:
+            convPass = convPass + i
+
+        return convPass
+
+        
+            
+
 
 
     # function to check if everything is set up
@@ -185,8 +147,10 @@ class ED:
         if(self.__pin == None):
             raise Exception("please set pin")
 
-        if((self.keySalt1 == None) and (self.keySalt2 == None)):
-            raise Exception("please set key salt")
+        if(self.__convPass == None):
+            raise Exception("please set password and pin")
+    
+
     
     # function to encrypt things
     def encrypter(self , stringToEncrypt):
@@ -195,101 +159,83 @@ class ED:
             
         stringToReturn = ""
 
-        convPass = self.convPassword()
+        # key
+        key = Fernet.generate_key()
+        
+        # conv key from bytes to str 
+        newKey = key.decode("utf-8")
 
-        if(self.outpass):
+        # encryting the key using the conv pass and keysalts
+        keyToAdd = onetimepad.encrypt(newKey , self.__convPass)
 
-            # key
-            key = Fernet.generate_key()
-            
-            # conv key from bytes to str 
-            newKey = key.decode("utf-8")
+        # conv string to bytes
+        stringToPass = bytes(stringToEncrypt , "utf-8")
 
-            # encryting the key using the conv pass and keysalts
-            keyToAdd = onetimepad.encrypt(newKey , self.keySalt1 + convPass + self.keySalt2)
+        cipher_suite = Fernet(key)
+        encoded_text = cipher_suite.encrypt(stringToPass)
+        stringToAdd = encoded_text.decode("utf-8")
 
-
-            # conv string to bytes
-            stringToPass = bytes(stringToEncrypt , "utf-8")
-
-            cipher_suite = Fernet(key)
-            encoded_text = cipher_suite.encrypt(stringToPass)
-            stringToAdd = encoded_text.decode("utf-8")
-
-            stringToReturn = keyToAdd + stringToAdd
-
-
-        else:
-
-            # password that will be added is encrypted by the convPass
-            passwordToAdd = onetimepad.encrypt(self.__password , convPass)
-
-            # key
-            key = Fernet.generate_key()
-            
-            # conv key from bytes to str 
-            newKey = key.decode("utf-8")
-
-            # encryting the key using the conv pass and keysalts
-            keyToAdd = onetimepad.encrypt(newKey , self.keySalt1 + convPass + self.keySalt2)
-
-
-            # conv string to bytes
-            stringToPass = bytes(stringToEncrypt , "utf-8")
-
-            cipher_suite = Fernet(key)
-            encoded_text = cipher_suite.encrypt(stringToPass)
-            stringToAdd = encoded_text.decode("utf-8")
-
-
-            stringToReturn = passwordToAdd + "////////////" + keyToAdd + "////////////" + stringToAdd
+        stringToReturn = keyToAdd + stringToAdd
 
         return stringToReturn
 
     
+
+
     def decrypter(self , stringToDecrypt):
 
         self.checkIfPossible()
 
-        convPass = self.convPassword()
+        # getting the key
+        newKey = onetimepad.decrypt(stringToDecrypt[:88] , self.__convPass)
 
-        if(isSubString(stringToDecrypt , "////////////")):
+        # conv strings to bytes
+        key = bytes(newKey , "utf-8")
 
-            myList = stringToDecrypt.split("////////////")
+        cipher_suite = Fernet(key)
+        decoded_text = cipher_suite.decrypt(bytes(stringToDecrypt[88:] , "utf-8"))
 
-            if(len(myList) != 3):
-                raise Exception("could not decrypt")
+        return decoded_text.decode("utf-8")
 
-            # checking if the password is correct or not
-            toComparePass = onetimepad.decrypt(myList[0] , convPass)
-            if(toComparePass != self.__password):
-                raise Exception("could not decrypt , password does not match")
 
-            # getting the key
-            newKey = onetimepad.decrypt(myList[1] , self.keySalt1 + convPass + self.keySalt2)
+    
+    def getEncryptedPassword(self , password):
+        sha_signature = hashlib.sha256(password.encode()).hexdigest()
+        return sha_signature
 
-            # conv strings to bytes
-            key = bytes(newKey , "utf-8")
+    
+    def returnPassForStoring(self):
+        return self.__convPass
 
-            cipher_suite = Fernet(key)
-            decoded_text = cipher_suite.decrypt(bytes(myList[2] , "utf-8"))
-
-            return decoded_text.decode("utf-8")
-
-        else:
-
-            # getting the key
-            newKey = onetimepad.decrypt(stringToDecrypt[:88] , self.keySalt1 + convPass + self.keySalt2)
-
-            # conv strings to bytes
-            key = bytes(newKey , "utf-8")
-
-            cipher_suite = Fernet(key)
-            decoded_text = cipher_suite.decrypt(bytes(stringToDecrypt[88:] , "utf-8"))
-
-            return decoded_text.decode("utf-8")
+    def authenticatePassword(self, storedPass , passwordInput , pinInput = 123456):
         
+        tempPass = self.__password
+        tempPin = self.__pin
+        tempConvPass = self.__convPass
 
+        self.setPassword_Pin(passwordInput , pinInput)
+
+        if(str(self.__convPass) == str(storedPass)):
+            self.__password = tempPass
+            self.__pin = tempPin
+            self.__convPass = tempConvPass
+
+            del tempPass
+            del tempPin
+            del tempConvPass
+
+            return True
+            
+        else:
+            self.__password = tempPass
+            self.__pin = tempPin
+            self.__convPass = tempConvPass
+
+            del tempPass
+            del tempPin
+            del tempConvPass
+
+            return False
 
         
 
@@ -345,8 +291,7 @@ for i in range(toDo):
 
     string = ""
 
-    rand = random.randint(256 , 1024)
-    rand2 = random.randint(4 , 16)
+    rand = random.randint(256 , 123456)
 
     for j in range(rand):
         string = string + str(random.choice(myList))
@@ -364,28 +309,13 @@ for i in range(toDo):
     for j in range(3):
         password = password + str(random.choice(nums))
 
-    salt = ""
-
-    for i in range(rand2):
-        salt = salt + str(random.choice(myList))
-
     
-    pin = random.randint(100000 , 999999)
+    pin = random.randint(0 , 999999)
 
-    saltList = []
-
-    for i in range(6):
-        saltstring = ""
-        for j in range(random.randint(4 , 16)):
-            saltstring = saltstring + str(random.choice(myList)) 
-        saltList.append(saltstring)
 
     try:
-        e.setPassword_Pin_keySalt(password , pin , salt)
-        e.setOwnSaltList(saltList)
-
+        e.setPassword_Pin(password , pin)
         enc = e.encrypter(string)
-
         dec = e.decrypter(enc)
 
         if(dec != string):
@@ -400,7 +330,6 @@ for i in range(toDo):
         tempList.append(ex)
         tempList.append(password)
         tempList.append(pin)
-        tempList.append(salt)
         tempList.append(string)
         exList.append(tempList)
 
